cbf0a083be1b5c181d9d05477d6516c1ddabb74b
Taehyeon Kim <97taehyun.com>
2023-08-30 11:57:38 +0900

    dd

---
+++ javawork/DataTypeEx1.java
@@ -0,0 +1,14 @@
+public class DataTypeEx1{
+	public static void main(String[] args){
+	
+		// 한줄 주석
+		/*
+			주석
+			주석
+		*/
+	//byte의 크기는 -128~127
+	 byte a=127;
+	 System.out.println("a="+a);
+
+	}
+}
