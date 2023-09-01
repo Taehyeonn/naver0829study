ef7fd4c8472b019beb0ed21d61e3d61c68fa28fb
taehyeon <97taehyun@gmail.com>
2023-09-01 17:21:44 +0900

    0901

--- javawork/THpractice/src/workjava/Day0901.java
+++ javawork/THpractice/src/workjava/Day0901.java
@@ -5,18 +5,18 @@ public class Day0901 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int jul=5;
+		int jul=9;
 		
 		for(int i=0; i<jul; i++) { // 1 3 5 7 9
-			
 			System.out.printf(" ".repeat(jul-i));
 			System.out.printf("*".repeat(1+2*i));
 			System.out.println();
 		}
 		
-		for(int i=jul; i>0; i--) {
-			System.out.println(i);
-//			System.out.println("*".repeat(5  1 )); // 9  7 5 3 1
+		for(int i=jul-1; i>0; i--) { // 4 3 2 1
+			System.out.printf(" ".repeat(1+jul-i));
+			System.out.printf("*".repeat(i*2-1));
+			System.out.println();
 		}
 		
 	}
